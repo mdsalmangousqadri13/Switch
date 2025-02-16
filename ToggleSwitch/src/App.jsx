6f6f3b16b395b91a74855fb30969efaf6383d8c3
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-02-16 21:06:57 +0530

    Initial commit

---
+++ ToggleSwitch/src/App.jsx
@@ -0,0 +1,11 @@
+import { DarkLight, ThemeProvider } from "./MiniProject/DarkLight"
+
+export const App = () => {
+  return (
+    <>
+     <ThemeProvider>
+      <DarkLight />
+     </ThemeProvider>
+    </>
+  )
+}
